3f023de166b47aaa96922e75b0972d1c076e713d
s28490 <s28490@pjwstk.edu.pl>
2024-03-09 17:38:09 +0100

    Add GetAverage method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,3 +7,17 @@ int a = 0;
 int b = 0;
 
 int c = 0;
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAverage(arr));
+
+static double GetAverage(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
