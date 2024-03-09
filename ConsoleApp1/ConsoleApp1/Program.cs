930ff0cba096d30124cda1d847367481a50139e9
s28490 <s28490@pjwstk.edu.pl>
2024-03-09 17:42:32 +0100

    Add Max Value  method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ int c = 0;
 
 int[] arr = { 1, 2, 3, 4, 5 };
 Console.WriteLine(GetAverage(arr));
+Console.WriteLine(maxValue(arr));
 
 static double GetAverage(int[] arr)
 {
@@ -20,4 +21,18 @@ static double GetAverage(int[] arr)
     }
 
     return sum / arr.Length;
+}
+
+static int maxValue(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
 }
